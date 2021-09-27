79e27c787e7c769806fc7fca2c16b7f20f749a26
vicente <test@fakemail.com>
2021-09-27 12:00:47 -0300

    Mapa

--- tesis/core/views.py
+++ tesis/core/views.py
@@ -16,10 +16,13 @@ def tabla(request):
     return render(request, 'prueba.html', context={'lat':latitud.to_list(), 'lon':longitud.to_list()})
 
 def tabla_dos(request):
-    documento=pd.read_table('core/20200925_Directorio_Oficial_EE_2020_20200430_WEB.csv', sep=';')
-    coquimbo=documento[documento['COD_REG_RBD']==4]
-    latitud=coquimbo['LATITUD'].dropna()
-    longitud=coquimbo['LONGITUD'].dropna()
-    lat=pd.to_numeric(lat,owncast='float')
-    
-    return render(request, 'prueba_dos.html', context={ 'cord': {'lat':lat.to_list(), 'lon':longitud.to_list()}})
+    coquimbo=pd.read_table('core/coquimbo.csv', sep=',',decimal=",")
+ 
+    latitud=coquimbo['LATITUD']
+    longitud=coquimbo['LONGITUD']
+
+    mylist = zip(latitud.to_list(), longitud.to_list())
+
+    context={ 'cord': mylist}
+
+    return render(request, 'prueba_dos.html',context )
